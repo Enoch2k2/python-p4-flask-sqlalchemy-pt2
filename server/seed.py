1b7d3c7f584a4d00b0a4ffe8e98c65071494ef61
Enoch2k2 <enoch2k2@hotmail.com>
2023-05-03 13:11:39 -0500

    finished

--- server/seed.py
+++ server/seed.py
@@ -6,3 +6,22 @@ from faker import Faker
 
 from app import app
 from models import db, Owner, Pet
+
+fake = Faker()
+
+with app.app_context():
+
+    Pet.query.delete()
+    Owner.query.delete()
+
+    owners = [Owner(name=fake.name()) for n in range(50)]
+
+    db.session.add_all(owners)
+
+    species = ['Dog', 'Cat', 'Chicken', 'Hamster', 'Turtle']
+
+    pets = [Pet(name=fake.first_name(), species=rc(
+        species), owner=rc(owners)) for n in range(100)]
+
+    db.session.add_all(pets)
+    db.session.commit()
